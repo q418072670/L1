9f1a28ca480cccae6b09d2fa98104c1ecfcf0197
QiuYuan Cai <coolpi07@gmail.com>
2017-04-19 16:46:05 +0800

    optimize index.js

--- ajaxdemo/script/index.js
+++ ajaxdemo/script/index.js
@@ -4,20 +4,26 @@ function $(id) {
 
 function bindEvent() {
     var b = [[],[]],
-        keyword = document.getElementById('keyword').value;
+        keyword = document.getElementById('keyword').value,
+        textCt = ["textWP","textMP"],
+        sexyCt = ["WL3","ML3"],
+        ab = $('ab').value;
     keyword = keyword.replace("搜索", "").replace(/\"/g,"").replace(/\s/g, "");
     var keywordArr = keyword.split("/");
     for (var i = 0; i < keywordArr.length; i++) {
         for (var j = 0; j < keywordArr[i].split("-").length; j++) {
             b[i][j] = keywordArr[i].split("-")[j];
         }
-    }
-    console.log(b)
-    var ab = $('ab').value;
-    render("textWP",b[0].join(""));
-    search_main("WL3", "WL3/add.php?wm=" + b[0][0] + "&name=" + b[0][1] + "&link=" + b[0][2] + "&dian=" + ab);
-    render("textMP",b[1].join(""));    
-    search_main("ML3", "WL3/add.php?wm=" + b[1][0] + "&name=" + b[1][1] + "&link=" + b[1][2] + "&dian=" + ab);
+    }        
+    textCt.forEach(function(currentvalue,index){
+        render(currentvalue,b[index].join(""));
+    })
+    sexyCt.forEach(function(currentValue,index){
+        search_main(currentvalue,"WL3/add.php?wm=" + b[index][0] + "&name=" + b[index][1] + "&link=" + b[index][2] + "&dian=" + ab)
+    })
+    // search_main("WL3", "WL3/add.php?wm=" + b[0][0] + "&name=" + b[0][1] + "&link=" + b[0][2] + "&dian=" + ab);
+      
+    // search_main("ML3", "WL3/add.php?wm=" + b[1][0] + "&name=" + b[1][1] + "&link=" + b[1][2] + "&dian=" + ab);
 }
 
 function render(textval,val){
