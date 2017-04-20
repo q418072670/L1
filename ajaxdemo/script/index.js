9116dcfbf32db612b079a24ad4d1844e4c7ad095
QiuYuan Cai <coolpi07@gmail.com>
2017-04-20 11:20:27 +0800

    V 0.1

--- ajaxdemo/script/index.js
+++ ajaxdemo/script/index.js
@@ -132,3 +132,59 @@ _('search').onclick = function(){
         }
 
 }()
+
+// 保存按钮逻辑
+_('saveBtn').addEventListener('click',function(e){
+    var shoplist = _('shoplist').value,
+        pageSelect = _('pageSelect').value,
+        pageSelectlist = _('pageSelectlist').value,
+        linkname = _('linkname').value,
+        linkaddress = _('linkaddress').value,
+        url;
+        if(pageSelect != 'TL'){
+            if(linkaddress.split('?')[1] && linkaddress != linkname){
+                url = linkaddress.split('?')[0] + '#' + linkaddress.split('?')[1].split('#')[1]; 
+            }else{
+                url = linaddress.split('?')[0]
+            }
+        }else{
+            if(shoplist == 'b'){
+                var bLink = linkaddress.split("scid=");
+                url = "http://www.uniqlo.cn/search.htm?scid=" + bLink[1].split('&')[0];
+            }else{
+                url = linkaddress.split('?')[0]
+            }
+        }
+        saveData(url,shoplist,pageSelect,pageSelectlist,linkname);
+})
+
+function saveData(link,shoplist,pageSelect,pageSelectlist,linkname){
+    var request = new XMLHttpRequest();	
+	request.open("POST", "WL3/add.php");
+	var data = "&dian=" + shoplist +
+				"&wm=" + pageSelect + 
+				"&biao=" + pageSelectlist + 
+				"&name=" + linkname + 
+				"&link=" + link;
+	request.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+	request.send(data);
+	request.onreadystatechange = function() {
+		if (request.readyState===4) {
+			if (request.status===200) { 
+				var data = JSON.parse(request.responseText);
+				if (data.success) { 
+					_("infoResult").innerHTML = data.msg;
+                    _("inforResult").display = "block";
+				} else {
+					_("infoResult").innerHTML = "出现错误：" + data.msg;
+                    _("infoResult").className = "alert alert-danger mT"
+                    _("inforResult").display = "block";
+				}
+			} else {
+				alert("发生错误：" + request.status);
+				}
+			} 
+		}
+
+
+}
