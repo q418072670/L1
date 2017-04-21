6a6e89b0f9f74ee18f54e3119f8db32b55443d71
xhj <418072670@qq.com>
2017-04-21 22:16:16 +0800

    updata for fenglei

--- ajaxdemo/WL3/search.php
+++ ajaxdemo/WL3/search.php
@@ -1,10 +1,25 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>无标题文档</title>
-</head>
-
-<body>
-</body>
-</html>
+<?php
+	require_once('../connect.php');
+	$wm= $_GET["wm"];
+	$name= $_GET["name"];
+	 $Array=array(array(  
+				'dian' => '123',
+           		 'name' => '123',
+           		 'link' => '123',)
+    		);
+	$i=0;
+	$mysqli->select_db("$wm");
+	$sql="SELECT * FROM `$name`";
+	if($result = $mysqli->query($sql)){
+		 while( $row = mysqli_fetch_assoc($result) ){  
+		    $Array[$i]['dian']=$row['dian'];
+			$Array[$i]['name']=$row['name'];
+			$Array[$i]['link']=$row['link'];
+			
+			
+		};
+		 echo json_encode($Array);
+		 //echo '{"success":true,"msg":"'.json_encode($Array).'"}';
+	}else{
+			echo '{"success":false,"msg":"失败"}';};
+?>
--- ajaxdemo/script/index.js
+++ ajaxdemo/script/index.js
@@ -1,26 +1,26 @@
 function _(id) {
     return document.getElementById(id);
 }
-
 function bindEvent() {
     var b = [[],[]],
         keyword = document.getElementById('keyword').value,
         textCt = ["textWP","textMP"],
         sexyCt = ["WL3","ML3"],
         ab = _('ab').value;
-    keyword = keyword.replace("搜索", "").replace(/\"/g,"").replace(/\/\s/, "\/");
+    keyword = keyword.replace("搜索", "").replace(/\"/g,"");
     var keywordArr = keyword.split("/");
     for (var i = 0; i < keywordArr.length; i++) {
         for (var j = 0; j < keywordArr[i].split("-").length; j++) {
-            b[i][j] = keywordArr[i].split("-")[j];
+            b[i][j] = keywordArr[i].split("-")[j].trim();
         }
-    }        
+    }
+    
     textCt.forEach(function(currentvalue,index){
         render(currentvalue,b[index].join(""));
-    })
+    });
     sexyCt.forEach(function(currentvalue,index){
         search_main(currentvalue,"WL3/add.php?wm=" + b[index][0] + "&name=" + b[index][1] + "&link=" + b[index][2] + "&dian=" + ab)
-    })
+    });
 }
 
 function render(textval,val){
@@ -29,7 +29,7 @@ function render(textval,val){
 }
 function search_main(obj, url) {
     var request = new XMLHttpRequest();
-    request.open("GET", url)
+    request.open("GET", url);
     request.send();
     request.onreadystatechange = function () {
         if (request.readyState === 4 && request.status === 200) {
@@ -37,7 +37,7 @@ function search_main(obj, url) {
             if (data.success) {
                 if (data.xianding) {
                     if(data.msg == '?' || data.msg == '&'){
-                        _(obj).value = '没有链接'
+                        _(obj).value = '没有链接';
                     }else{
                         _(obj).value = data.msg + 'keyword=' + UrlEncode(data.text);    
                     }                     
@@ -48,23 +48,27 @@ function search_main(obj, url) {
                 _(obj).value = "出现错误：" + data.msg;
             }
         }
-    }
+    };
 }
 
 
 
 _('search').onclick = function(){
     bindEvent();
-}
+};
 
 // 模态框逻辑
 !function(){
     var pageSelect = _('pageSelect'),
         pageSelectlist = _('pageSelectlist'),
-        pageSelectOption = {
+         pageSelectOption = {
             "--请选择类别--": "",
             "WL3":"WL3",
             "ML3":"ML3",
+			"KL3":"KL3",
+			"GL3":"GL3",
+			"BL3":"BL3",
+			"BABYL3":"BABYL3",
             "通栏":"TL"
         },
         pageSelectlistOption = {            
@@ -89,15 +93,43 @@ _('search').onclick = function(){
                 "长裤": "长裤",
 				"束脚裤":'束脚裤'
             },
+			"kid":{
+                "起居服": "起居服",
+                "AIRism": "AIRism",
+                "运动特辑": "运动特辑",
+                "UT特辑": "UT特辑"
+            },
+			"girl":{
+                "女童上装": "女童上装",
+                "女童裤装": "女童裤装"
+            },
+			"boy":{
+                "男童上装": "男童上装",
+                "男童裤装": "男童裤装"
+            },
+			"baby":{
+                "新生儿": "新生儿",
+                "幼儿上装": "幼儿上装",
+                "幼儿下装": "幼儿下装",
+                "幼儿内衣": "幼儿内衣"
+            },
             "限定": {
                 "W限定L3": "W限定L3",
-                "M限定L3": "M限定L3"
+                "M限定L3": "M限定L3",
+				"W全商品L3": "W全商品L3",
+                "M全商品L3": "M全商品L3",
+				"W超值精选L3": "W超值精选L3",
+                "M超值精选L3": "M超值精选L3"
             }
-        }
+        },
         pageSelectArr = [],
         pageSelectlistArr = [],
         womenlistArr = [],
         menlistArr = [],
+		kidlistArr = [],
+		girllistArr = [],
+		boylistArr = [],
+		babylistArr = [],
         limitedlistArr = [];
 
         for(var key in pageSelectOption){
@@ -109,12 +141,20 @@ _('search').onclick = function(){
 
         pushArr(pageSelectlistOption["women"],womenlistArr);
         pushArr(pageSelectlistOption["men"],menlistArr);
+		pushArr(pageSelectlistOption["kid"],kidlistArr);
+		pushArr(pageSelectlistOption["girl"],girllistArr);
+		pushArr(pageSelectlistOption["boy"],boylistArr);
+		pushArr(pageSelectlistOption["baby"],babylistArr);
         pushArr(pageSelectlistOption["限定"],limitedlistArr);
        
 
         pageSelectlistArr.push(new Array(new Option("--请选择--","")))
         pageSelectlistArr.push(womenlistArr)
         pageSelectlistArr.push(menlistArr)
+		pageSelectlistArr.push(kidlistArr)
+		pageSelectlistArr.push(girllistArr)
+		pageSelectlistArr.push(boylistArr)
+		pageSelectlistArr.push(babylistArr)
         pageSelectlistArr.push(limitedlistArr)
         
         pageSelect.addEventListener('change',function(e){
@@ -123,11 +163,11 @@ _('search').onclick = function(){
             for(var i = 0; i< pageSelectlistArr[index].length;i++){
                 pageSelectlist.options[i] = pageSelectlistArr[index][i];
             }        
-        })
+        });
 
         function pushArr(obj,arr){
             for(var key in obj){
-                arr.push(new Option(key,obj[key]))
+                arr.push(new Option(key,obj[key]));
             }
         }
 
@@ -141,22 +181,29 @@ _('saveBtn').addEventListener('click',function(e){
         linkname = _('linkname').value,
         linkaddress = _('linkaddress').value,
         url;
-        if(pageSelect != 'TL'){
-            if(linkaddress.split('?')[1] && linkaddress != linkname){
-                url = linkaddress.split('?')[0] + '#' + linkaddress.split('?')[1].split('#')[1]; 
+       if(linkaddress.match(/^http/)){
+            if(pageSelect != 'TL'){
+                if(linkaddress.split('?')[1] && pageSelectlist != linkname){
+                    url = linkaddress.split('?')[0] + '#' + linkaddress.split('?')[1].split('#')[1]; 
+                }else{
+                    url = linkaddress.split('?')[0];
+                }
             }else{
-                url = linkaddress.split('?')[0]
+                if(shoplist == 'b'){
+                    var bLink = linkaddress.split("scid=");
+                    url = "http://www.uniqlo.cn/search.htm?scid=" + bLink[1].split('&')[0];
+                }else{
+                    url = linkaddress.split('?')[0];
+                }
             }
         }else{
-            if(shoplist == 'b'){
-                var bLink = linkaddress.split("scid=");
-                url = "http://www.uniqlo.cn/search.htm?scid=" + bLink[1].split('&')[0];
-            }else{
-                url = linkaddress.split('?')[0]
-            }
+            infoResult.innerText = '请输入正确的网址信息';
+            infoResult.className = "alert alert-danger mT";
+            infoResult.style.display = "block";
+			return;
         }
         saveData(url,shoplist,pageSelect,pageSelectlist,linkname);
-})
+});
 
 function saveData(link,shoplist,pageSelect,pageSelectlist,linkname){
     var request = new XMLHttpRequest();	
@@ -173,18 +220,19 @@ function saveData(link,shoplist,pageSelect,pageSelectlist,linkname){
 			if (request.status===200) { 
 				var data = JSON.parse(request.responseText);
 				if (data.success) { 
+					_("infoResult").style.display = "block";
 					_("infoResult").innerHTML = data.msg;
-                    _("inforResult").display = "block";
+                    
 				} else {
 					_("infoResult").innerHTML = "出现错误：" + data.msg;
-                    _("infoResult").className = "alert alert-danger mT"
-                    _("inforResult").display = "block";
+                    _("infoResult").className = "alert alert-danger mT";
+                    _("infoResult").style.display = "block";
 				}
 			} else {
 				alert("发生错误：" + request.status);
 				}
 			} 
-		}
+		};
 
 
 }
